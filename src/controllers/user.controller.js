9b007ae972010bfb812fac987eef1e83a2697f65
Vishal Jaiswal <kumardenzel@gmail.com>
2026-02-06 15:44:02 +0530

    Refactor getUserChannelProfile function to remove asyncHandler and improve readability

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -233,7 +233,7 @@ const updateCoverImage=async(req,res)=>{
 
 
 
-const getUserChannelProfile = asyncHandler(async(req, res) => {
+const getUserChannelProfile = async(req, res) => {
     const {username} = req.params
 
     if (!username?.trim()) {
@@ -303,9 +303,63 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
     .json(
         new ApiResponse(200, channel[0], "User channel fetched successfully")
     )
-})
+};
 
 
+
+const getWatchHistory = async(req, res) => {
+    const user = await UserModel.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner:{
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
+};
 
 export {
     RegisterUser,
